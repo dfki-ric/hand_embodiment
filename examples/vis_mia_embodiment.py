1be09311b3077093c07a942d2a780f9b224e2844
Alexander Fabisch <afabisch@googlemail.com>
2021-04-14 13:04:03 +0200

    Prepare multiple fingers

--- examples/vis_mia_embodiment.py
+++ examples/vis_mia_embodiment.py
@@ -45,7 +45,7 @@ index_tip2miabase, q = emb.solve()
 
 print(time.time() - start)
 
-pose = emb.target_finger_chains["index"].forward(q)
+pose = emb.target_finger_chains["index"].forward(q["index"])
 fig.plot_sphere(0.005, index_tip2miabase, c=(0, 0, 0))
 fig.plot_sphere(0.005, pose, c=(1, 0, 0))
 
--- hand_embodiment/embodiment.py
+++ hand_embodiment/embodiment.py
@@ -6,26 +6,35 @@ import pytransform3d.transformations as pt
 
 
 class HandEmbodiment:
-    def __init__(self, hand_state, target_config, mano_finger_kinematics=None):
+    def __init__(
+            self, hand_state, target_config,
+            use_fingers=("thumb", "index", "middle"),
+            mano_finger_kinematics=None):
         self.hand_state = hand_state
         if mano_finger_kinematics is None:
-            self.mano_finger_kinematics = {
-                "thumb": make_finger_kinematics(self.hand_state, "thumb"),
-                "index": make_finger_kinematics(self.hand_state, "index"),
-                "middle": make_finger_kinematics(self.hand_state, "middle")
-            }
+            self.mano_finger_kinematics = {}
+            for finger_name in use_fingers:
+                self.mano_finger_kinematics[finger_name] = \
+                    make_finger_kinematics(self.hand_state, finger_name)
         else:
             self.mano_finger_kinematics = mano_finger_kinematics
         self.handbase2robotbase = target_config["handbase2robotbase"]
+        for finger_name in use_fingers:
+            assert finger_name in self.mano_finger_kinematics
 
         self.target_kin = load_kinematic_model(target_config)
-        self.target_finger_chains = {
-            "index": self.target_kin.create_chain(
-                target_config["joint_names"]["index"],
-                target_config["base_frame"],
-                target_config["ee_frames"]["index"])
-        }
-        self.q = np.zeros(len(target_config["joint_names"]["index"]))
+        self.target_finger_chains = {}
+        self.joint_angles = {}
+        for finger_name in use_fingers:
+            assert finger_name in target_config["joint_names"]
+            assert finger_name in target_config["ee_frames"]
+            self.target_finger_chains[finger_name] = \
+                self.target_kin.create_chain(
+                    target_config["joint_names"][finger_name],
+                    target_config["base_frame"],
+                    target_config["ee_frames"][finger_name])
+            self.joint_angles[finger_name] = \
+                np.zeros(len(target_config["joint_names"][finger_name]))
 
     def solve(self):
         # TODO for each finger
@@ -33,8 +42,8 @@ class HandEmbodiment:
             self.hand_state.pose[self.mano_finger_kinematics["index"].finger_pose_param_indices])
         index_tip_in_handbase = pt.transform(
             self.handbase2robotbase, pt.vector_to_point(index_tip_in_manobase))
-        self.q = self.target_finger_chains["index"].inverse_position(index_tip_in_handbase[:3], self.q)
-        return pt.translate_transform(np.eye(4), index_tip_in_handbase), self.q
+        self.joint_angles["index"] = self.target_finger_chains["index"].inverse_position(index_tip_in_handbase[:3], self.joint_angles["index"])
+        return pt.translate_transform(np.eye(4), index_tip_in_handbase), self.joint_angles
 
 
 def load_kinematic_model(hand_config):
