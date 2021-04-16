b051047e8a34ba7eee554ef685951755e676130c
Alexander Fabisch <afabisch@googlemail.com>
2021-04-16 18:00:20 +0200

    Compare shape to markers

--- examples/gui_mano_shape.py
+++ examples/gui_mano_shape.py
@@ -3,6 +3,9 @@ import numpy as np
 import open3d as o3d
 from open3d.visualization import gui
 from mocap.mano import HandState
+import pytransform3d.transformations as pt
+
+from hand_embodiment.record_markers import MarkerBasedRecordMapping
 from hand_embodiment.vis_utils import make_coordinate_system
 
 
@@ -112,6 +115,40 @@ fig = Figure("MANO shape", 1920, 1080, ax_s=0.2)
 
 hand_state = HandState(left=False)
 fig.add_hand_mesh(hand_state.hand_mesh, hand_state.material)
+
+import glob
+from mocap import qualisys, pandas_utils, cleaning, conversion
+pattern = "data/Qualisys_pnp/*.tsv"
+demo_idx = 2
+filename = list(sorted(glob.glob(pattern)))[demo_idx]
+trajectory = qualisys.read_qualisys_tsv(filename=filename)
+hand_trajectory = pandas_utils.extract_markers(trajectory, ["Hand left", "Hand right", "Hand top", "Middle", "Index", "Thumb"])
+hand_trajectory = hand_trajectory.iloc[100:200]
+hand_trajectory = cleaning.median_filter(cleaning.interpolate_nan(hand_trajectory), 3).iloc[2:]
+hand_left = conversion.array_from_dataframe(hand_trajectory, ["Hand left X", "Hand left Y", "Hand left Z"])
+hand_right = conversion.array_from_dataframe(hand_trajectory, ["Hand right X", "Hand right Y", "Hand right Z"])
+hand_top = conversion.array_from_dataframe(hand_trajectory, ["Hand top X", "Hand top Y", "Hand top Z"])
+middle = conversion.array_from_dataframe(hand_trajectory, ["Middle X", "Middle Y", "Middle Z"])
+index = conversion.array_from_dataframe(hand_trajectory, ["Index X", "Index Y", "Index Z"])
+thumb = conversion.array_from_dataframe(hand_trajectory, ["Thumb X", "Thumb Y", "Thumb Z"])
+t = 0
+mbrm = MarkerBasedRecordMapping(left=False)
+mbrm.estimate(
+    [hand_top[t], hand_left[t], hand_right[t]],
+    {"thumb": thumb[t], "index": index[t], "middle": middle[t]})
+world2mano = pt.invert_transform(mbrm.mano2world_)
+markers_in_world = np.array([hand_top[t], hand_left[t], hand_right[t], thumb[t], index[t], middle[t]])
+markers_in_mano = pt.transform(world2mano, pt.vectors_to_points(markers_in_world))[:, :3]
+
+for p in markers_in_mano:
+    marker = o3d.geometry.TriangleMesh.create_sphere(radius=0.005)
+    marker.compute_vertex_normals()
+    n_vertices = len(marker.vertices)
+    colors = np.zeros((n_vertices, 3))
+    colors[:] = (0.3, 0.3, 0.3)
+    marker.vertex_colors = o3d.utility.Vector3dVector(colors)
+    marker.translate(p)
+    fig.add_geometry(marker)
 
 coordinate_system = make_coordinate_system(s=0.2)
 fig.add_geometry(coordinate_system)
