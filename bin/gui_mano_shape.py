0d424abca892e463bb61f0217d9795acf40c53e4
Alexander Fabisch <afabisch@googlemail.com>
2021-05-31 21:22:28 +0200

    Clean up

--- bin/gui_mano_shape.py
+++ bin/gui_mano_shape.py
@@ -157,77 +157,79 @@ class OnMano:
 class OnManoChange(OnMano):
     def __init__(self, fig, mbrm):
         super(OnManoChange, self).__init__(fig, mbrm)
-        initial_pose = pt.exponential_coordinates_from_transform(
-            mbrm.mano2hand_markers_)
-        self.pose = initial_pose.copy()
-        self.shape = mbrm.hand_state_.betas.copy()
         self.mbrm = mbrm
+        self.update_mesh()
         self.redraw()
 
     def shape_changed(self, value, i):
-        self.shape[i] = value
         self.mbrm.hand_state_.betas[i] = value
         self.update_mesh()
         self.redraw()
 
     def pos_changed(self, value, i):
-        self.pose[i] = value
+        pose = pt.exponential_coordinates_from_transform(self.mbrm.mano2hand_markers_)
+        pose[i] = value
+        self.mbrm.mano2hand_markers_ = pt.transform_from_exponential_coordinates(pose)
         self.update_mesh()
         self.redraw()
 
     def update_mesh(self):
         self.mbrm.hand_state_.pose_parameters["J"], self.mbrm.hand_state_.pose_parameters["v_template"] = \
             mano.apply_shape_parameters(betas=self.mbrm.hand_state_.betas, **self.mbrm.hand_state_.shape_parameters)
-        mbrm.mano2hand_markers_ = pt.transform_from_exponential_coordinates(self.pose)
         self.mbrm.hand_state_.vertices[:, :] = mano.hand_vertices(
             pose=self.mbrm.hand_state_.pose, **self.mbrm.hand_state_.pose_parameters)
         self.mbrm.hand_state_.vertices[:, :] = pt.transform(
-            mbrm.mano2hand_markers_, pt.vectors_to_points(self.mbrm.hand_state_.vertices))[:, :3]
+            self.mbrm.mano2hand_markers_, pt.vectors_to_points(self.mbrm.hand_state_.vertices))[:, :3]
         self.mbrm.hand_state_._mesh.vertices = o3d.utility.Vector3dVector(self.mbrm.hand_state_.vertices)
         self.mbrm.hand_state_._mesh.compute_vertex_normals()
         self.mbrm.hand_state_._mesh.compute_triangle_normals()
 
 
-args = parse_args()
+def main():
+    args = parse_args()
 
-finger_names = ["thumb", "index", "middle", "ring"]
-hand_marker_names = ["hand_top", "hand_left", "hand_right"]
-finger_marker_names = {"thumb": "thumb_tip", "index": "index_tip",
-                       "middle": "middle_tip", "ring": "ring_tip"}
-additional_marker_names = ["index_middle", "middle_middle", "ring_middle"]
-dataset = HandMotionCaptureDataset(
-    args.mocap_filename, finger_names, hand_marker_names, finger_marker_names,
-    additional_marker_names)
-config_filename = args.filename
-if os.path.exists(config_filename):
-    mano2hand_markers, betas = load_mano_config(config_filename)
-else:
-    mano2hand_markers, betas = np.eye(4), np.zeros(10)
+    finger_names = ["thumb", "index", "middle", "ring"]
+    hand_marker_names = ["hand_top", "hand_left", "hand_right"]
+    finger_marker_names = {"thumb": "thumb_tip", "index": "index_tip",
+                           "middle": "middle_tip", "ring": "ring_tip"}
+    additional_marker_names = ["index_middle", "middle_middle", "ring_middle"]
+    dataset = HandMotionCaptureDataset(
+        args.mocap_filename, finger_names, hand_marker_names, finger_marker_names,
+        additional_marker_names)
+    config_filename = args.filename
+    if os.path.exists(config_filename):
+        mano2hand_markers, betas = load_mano_config(config_filename)
+    else:
+        mano2hand_markers, betas = np.eye(4), np.zeros(10)
 
-mbrm = MarkerBasedRecordMapping(
-    left=False, shape_parameters=betas, mano2hand_markers=mano2hand_markers)
-mbrm.estimate(dataset.get_hand_markers(args.start_idx), finger_markers={})  # TODO fit fingers?
-world2mano = pt.invert_transform(mbrm.mano2world_)
-markers_in_world = dataset.get_markers(args.start_idx)
-markers_in_mano = pt.transform(mano2hand_markers, pt.transform(world2mano, pt.vectors_to_points(markers_in_world)))[:, :3]
+    mbrm = MarkerBasedRecordMapping(
+        left=False, shape_parameters=betas, mano2hand_markers=mano2hand_markers)
+    mbrm.estimate(dataset.get_hand_markers(args.start_idx), finger_markers={})  # TODO fit fingers?
+    world2mano = pt.invert_transform(mbrm.mano2world_)
+    markers_in_world = dataset.get_markers(args.start_idx)
+    markers_in_mano = pt.transform(mano2hand_markers, pt.transform(world2mano, pt.vectors_to_points(markers_in_world)))[:, :3]
 
-fig = Figure("MANO shape", 1920, 1080, config_filename, ax_s=0.2)
-for p in markers_in_mano:  # TODO refactor
-    marker = o3d.geometry.TriangleMesh.create_sphere(radius=0.005)
-    n_vertices = len(marker.vertices)
-    colors = np.zeros((n_vertices, 3))
-    colors[:] = (0.3, 0.3, 0.3)
-    marker.vertex_colors = o3d.utility.Vector3dVector(colors)
-    marker.translate(p)
-    marker.compute_vertex_normals()
-    marker.compute_triangle_normals()
-    fig.add_geometry(marker)
+    fig = Figure("MANO shape", 1920, 1080, config_filename, ax_s=0.2)
+    for p in markers_in_mano:  # TODO refactor
+        marker = o3d.geometry.TriangleMesh.create_sphere(radius=0.005)
+        n_vertices = len(marker.vertices)
+        colors = np.zeros((n_vertices, 3))
+        colors[:] = (0.3, 0.3, 0.3)
+        marker.vertex_colors = o3d.utility.Vector3dVector(colors)
+        marker.translate(p)
+        marker.compute_vertex_normals()
+        marker.compute_triangle_normals()
+        fig.add_geometry(marker)
 
-coordinate_system = make_coordinate_system(s=0.2)
-fig.add_geometry(coordinate_system)
+    coordinate_system = make_coordinate_system(s=0.2)
+    fig.add_geometry(coordinate_system)
 
-#initial_pose = pt.transform_from_exponential_coordinates(np.array([-0.103, 1.97, -0.123, -0.066, -0.034, 0.083]))
-#initial_pose = pt.exponential_coordinates_from_transform(initial_pose)
-fig.make_mano_widgets(mbrm)
+    #initial_pose = pt.transform_from_exponential_coordinates(np.array([-0.103, 1.97, -0.123, -0.066, -0.034, 0.083]))
+    #initial_pose = pt.exponential_coordinates_from_transform(initial_pose)
+    fig.make_mano_widgets(mbrm)
 
-fig.show()
+    fig.show()
+
+
+if __name__ == "__main__":
+    main()
