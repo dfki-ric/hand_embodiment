54ffae7f9c0e5738ce9e18b7a527d0d1e711de6e
Alexander Fabisch <afabisch@googlemail.com>
2021-06-09 15:26:59 +0200

    Update conversion script to new data

--- bin/save_hand_trajectory.py
+++ bin/save_hand_trajectory.py
@@ -1,20 +1,24 @@
 """
 Example call:
 
-python bin/save_hand_trajectory.py mia --output trajectory.csv --start-idx 8000 --end-idx 8700
+python bin/save_hand_trajectory.py mia --mia-thumb-adducted --demo-file data/QualisysAprilTest/april_test_009.tsv --output trajectory_009.csv
 """
 import argparse
+import time
+import glob
 import pandas as pd
 import numpy as np
 from pytransform3d import transformations as pt
-import glob
+import tqdm
 from mocap import qualisys
 from mocap import pandas_utils
 from mocap.cleaning import interpolate_nan, median_filter
 from mocap import conversion
 from hand_embodiment.record_markers import MarkerBasedRecordMapping
 from hand_embodiment.embodiment import HandEmbodiment
+from hand_embodiment.mocap_dataset import HandMotionCaptureDataset
 from hand_embodiment.target_configurations import MIA_CONFIG, SHADOW_HAND_CONFIG
+from hand_embodiment.config import load_mano_config
 
 
 def parse_args():
@@ -26,17 +30,21 @@ def parse_args():
         "--output", type=str, default="trajectory.csv",
         help="Output file (.csv).")
     parser.add_argument(
-        "--start-idx", type=int, default=0, help="Start index.")
-    parser.add_argument(
-        "--end-idx", type=int, default=None, help="Start index.")
+        "--demo-file", type=str,
+        default="data/QualisysAprilTest/april_test_010.tsv",
+        help="Demonstration that should be used.")
+    #parser.add_argument(
+    #    "--start-idx", type=int, default=0, help="Start index.")
+    #parser.add_argument(
+    #    "--end-idx", type=int, default=None, help="Start index.")
     parser.add_argument(
         "--show-mano", action="store_true", help="Show MANO mesh")
     parser.add_argument(
         "--skip-frames", type=int, default=15,
         help="Skip this number of frames between animated frames.")
     parser.add_argument(
-        "--demo-idx", type=int, default=2,
-        help="Index of demonstration that should be used.")
+        "--mia-thumb-adducted", action="store_true",
+        help="Adduct thumb of Mia hand.")
 
     return parser.parse_args()
 
@@ -44,24 +52,15 @@ def parse_args():
 args = parse_args()
 
 
-pattern = "data/Qualisys_pnp/*.tsv"
-filename = list(sorted(glob.glob(pattern)))[args.demo_idx]
-trajectory = qualisys.read_qualisys_tsv(filename=filename)
-
-hand_trajectory = pandas_utils.extract_markers(trajectory, ["Hand left", "Hand right", "Hand top", "Middle", "Index", "Thumb"])
-hand_trajectory = hand_trajectory.iloc[
-                  args.start_idx:args.end_idx:args.skip_frames]
-
-hand_trajectory = median_filter(interpolate_nan(hand_trajectory), 3).iloc[2:]
-
-hand_left = conversion.array_from_dataframe(hand_trajectory, ["Hand left X", "Hand left Y", "Hand left Z"])
-hand_right = conversion.array_from_dataframe(hand_trajectory, ["Hand right X", "Hand right Y", "Hand right Z"])
-hand_top = conversion.array_from_dataframe(hand_trajectory, ["Hand top X", "Hand top Y", "Hand top Z"])
-middle = conversion.array_from_dataframe(hand_trajectory, ["Middle X", "Middle Y", "Middle Z"])
-index = conversion.array_from_dataframe(hand_trajectory, ["Index X", "Index Y", "Index Z"])
-thumb = conversion.array_from_dataframe(hand_trajectory, ["Thumb X", "Thumb Y", "Thumb Z"])
-
-n_steps = len(hand_top)
+filename = args.demo_file
+finger_names = ["thumb", "index", "middle", "ring"]
+hand_marker_names = ["hand_top", "hand_left", "hand_right"]
+finger_marker_names = {"thumb": "thumb_tip", "index": "index_tip",
+                       "middle": "middle_tip", "ring": "ring_tip"}
+additional_marker_names = ["index_middle", "middle_middle", "ring_middle"]
+dataset = HandMotionCaptureDataset(
+    filename, finger_names, hand_marker_names, finger_marker_names, additional_marker_names,
+    skip_frames=2)
 
 if args.hand == "shadow_hand":
     hand_config = SHADOW_HAND_CONFIG
@@ -70,37 +69,55 @@ elif args.hand == "mia":
 else:
     raise Exception(f"Unknown hand: '{args.hand}'")
 
-mano2hand_markers = pt.transform_from_exponential_coordinates([0.048, 1.534, -0.092, -0.052, -0.031, 0.045])
-betas = np.array([-2.424, -1.212, -1.869, -1.616, -4.091, -1.768, -0.808, 2.323, 1.111, 1.313])
-
-hse = MarkerBasedRecordMapping(
+mano2hand_markers, betas = load_mano_config(
+    "examples/config/april_test_mano.yaml")
+use_fingers = ("thumb", "index", "middle", "ring")
+mbrm = MarkerBasedRecordMapping(
     left=False, mano2hand_markers=mano2hand_markers, shape_parameters=betas,
-    verbose=1)
-use_fingers = list(hse.mano_finger_kinematics_.keys())
+    verbose=0)
 emb = HandEmbodiment(
-    hse.hand_state_, hand_config, mano_finger_kinematics=hse.mano_finger_kinematics_,
-    initial_handbase2world=hse.mano2world_, verbose=1)
+    mbrm.hand_state_, hand_config,
+    use_fingers=use_fingers,
+    mano_finger_kinematics=mbrm.mano_finger_kinematics_,
+    initial_handbase2world=mbrm.mano2world_, verbose=0)
+if args.hand == "mia":
+    if args.mia_thumb_adducted:
+        emb.target_kin.tm.set_joint("j_thumb_opp_binary", 1.0)
+    else:
+        emb.target_kin.tm.set_joint("j_thumb_opp_binary", -1.0)
 
 all_joint_angles = []
 all_ee_poses = []
-for t in range(n_steps):
-    hse.estimate(
-        [hand_top[t], hand_left[t], hand_right[t]],
-        {"thumb": thumb[t], "index": index[t], "middle": middle[t]})
-    joint_angles = emb.solve(hse.mano2world_, use_cached_forward_kinematics=True)
+start_time = time.time()
+for t in tqdm.tqdm(range(dataset.n_steps)):
+    mbrm.estimate(dataset.get_hand_markers(t), dataset.get_finger_markers(t))
+    joint_angles = emb.solve(mbrm.mano2world_, use_cached_forward_kinematics=True)
     ee_pose = emb.transform_manager_.get_transform(
         hand_config["base_frame"], "world")
-    all_joint_angles.append(joint_angles.copy())
+    joint_angles_t = joint_angles.copy()
+    if args.hand == "mia":
+        j_min, j_max = emb.transform_manager_.get_joint_limits("j_thumb_opp")
+        if args.mia_thumb_adducted:
+            thumb_opp = j_max
+        else:
+            thumb_opp = j_min
+        joint_angles_t["thumb"] = np.hstack((joint_angles_t["thumb"], (thumb_opp,)))
+    all_joint_angles.append(joint_angles_t)
     all_ee_poses.append(ee_pose)
+stop_time = time.time()
+duration = stop_time - start_time
+print(f"Embodiment mapping done after {duration} s, {duration / dataset.n_steps} s per frame, {dataset.n_steps / duration} Hz")
 
 pose_columns = ["base_x", "base_y", "base_z", "base_qw", "base_qx", "base_qy", "base_qz"]
 column_names = []
 for finger in use_fingers:
     column_names += hand_config["joint_names"][finger]
+    if args.hand == "mia" and finger == "thumb":
+        column_names.append("j_thumb_opp")
 column_names += pose_columns
 
 raw_data = []
-for t in range(n_steps):
+for t in range(dataset.n_steps):
     joint_angles = []
     for finger in use_fingers:
         joint_angles += all_joint_angles[t][finger].tolist()
