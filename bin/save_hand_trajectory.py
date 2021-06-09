980a900f554f5d5ec357eeb0bb66f879f0a136bc
Alexander Fabisch <afabisch@googlemail.com>
2021-06-09 18:09:15 +0200

    Refactor conversion script

--- bin/save_hand_trajectory.py
+++ bin/save_hand_trajectory.py
@@ -44,74 +44,85 @@ def parse_args():
     return parser.parse_args()
 
 
-args = parse_args()
+def main():
+    args = parse_args()
 
-finger_names = ["thumb", "index", "middle", "ring"]
-hand_marker_names = ["hand_top", "hand_left", "hand_right"]
-finger_marker_names = {"thumb": "thumb_tip", "index": "index_tip",
-                       "middle": "middle_tip", "ring": "ring_tip"}
-additional_marker_names = ["index_middle", "middle_middle", "ring_middle"]
-dataset = HandMotionCaptureDataset(
-    args.demo_file, finger_names, hand_marker_names, finger_marker_names,
-    additional_marker_names, skip_frames=args.skip_frames,
-    start_idx=args.start_idx, end_idx=args.end_idx)
+    finger_names = ["thumb", "index", "middle", "ring"]
+    hand_marker_names = ["hand_top", "hand_left", "hand_right"]
+    finger_marker_names = {"thumb": "thumb_tip", "index": "index_tip",
+                           "middle": "middle_tip", "ring": "ring_tip"}
+    additional_marker_names = ["index_middle", "middle_middle", "ring_middle"]
+    dataset = HandMotionCaptureDataset(
+        args.demo_file, finger_names, hand_marker_names, finger_marker_names,
+        additional_marker_names, skip_frames=args.skip_frames,
+        start_idx=args.start_idx, end_idx=args.end_idx)
 
-hand_config = TARGET_CONFIG[args.hand]
+    hand_config = TARGET_CONFIG[args.hand]
 
-mano2hand_markers, betas = load_mano_config(
-    "examples/config/april_test_mano.yaml")
-use_fingers = ("thumb", "index", "middle", "ring")
-mbrm = MarkerBasedRecordMapping(
-    left=False, mano2hand_markers=mano2hand_markers, shape_parameters=betas,
-    verbose=0)
-emb = HandEmbodiment(
-    mbrm.hand_state_, hand_config,
-    use_fingers=use_fingers,
-    mano_finger_kinematics=mbrm.mano_finger_kinematics_,
-    initial_handbase2world=mbrm.mano2world_, verbose=0)
-if args.hand == "mia":
-    if args.mia_thumb_adducted:
-        emb.target_kin.tm.set_joint("j_thumb_opp_binary", 1.0)
-    else:
-        emb.target_kin.tm.set_joint("j_thumb_opp_binary", -1.0)
-
-all_joint_angles = []
-all_ee_poses = []
-start_time = time.time()
-for t in tqdm.tqdm(range(dataset.n_steps)):
-    mbrm.estimate(dataset.get_hand_markers(t), dataset.get_finger_markers(t))
-    joint_angles = emb.solve(mbrm.mano2world_, use_cached_forward_kinematics=True)
-    ee_pose = emb.transform_manager_.get_transform(
-        hand_config["base_frame"], "world")
-    joint_angles_t = joint_angles.copy()
+    mano2hand_markers, betas = load_mano_config(
+        "examples/config/april_test_mano.yaml")
+    use_fingers = ("thumb", "index", "middle", "ring")
+    mbrm = MarkerBasedRecordMapping(
+        left=False, mano2hand_markers=mano2hand_markers, shape_parameters=betas,
+        verbose=0)
+    emb = HandEmbodiment(
+        mbrm.hand_state_, hand_config,
+        use_fingers=use_fingers,
+        mano_finger_kinematics=mbrm.mano_finger_kinematics_,
+        initial_handbase2world=mbrm.mano2world_, verbose=0)
     if args.hand == "mia":
-        j_min, j_max = emb.transform_manager_.get_joint_limits("j_thumb_opp")
         if args.mia_thumb_adducted:
-            thumb_opp = j_max
+            emb.target_kin.tm.set_joint("j_thumb_opp_binary", 1.0)
         else:
-            thumb_opp = j_min
-        joint_angles_t["thumb"] = np.hstack((joint_angles_t["thumb"], (thumb_opp,)))
-    all_joint_angles.append(joint_angles_t)
-    all_ee_poses.append(ee_pose)
-stop_time = time.time()
-duration = stop_time - start_time
-print(f"Embodiment mapping done after {duration} s, {duration / dataset.n_steps} s per frame, {dataset.n_steps / duration} Hz")
+            emb.target_kin.tm.set_joint("j_thumb_opp_binary", -1.0)
 
-pose_columns = ["base_x", "base_y", "base_z", "base_qw", "base_qx", "base_qy", "base_qz"]
-column_names = []
-for finger in use_fingers:
-    column_names += hand_config["joint_names"][finger]
-    if args.hand == "mia" and finger == "thumb":
-        column_names.append("j_thumb_opp")
-column_names += pose_columns
+    all_joint_angles = []
+    all_ee_poses = []
+    start_time = time.time()
+    for t in tqdm.tqdm(range(dataset.n_steps)):
+        mbrm.estimate(dataset.get_hand_markers(t),
+                      dataset.get_finger_markers(t))
+        joint_angles = emb.solve(
+            mbrm.mano2world_, use_cached_forward_kinematics=True)
+        ee_pose = emb.transform_manager_.get_transform(
+            hand_config["base_frame"], "world")
+        joint_angles_t = joint_angles.copy()
+        if args.hand == "mia":
+            j_min, j_max = emb.transform_manager_.get_joint_limits("j_thumb_opp")
+            if args.mia_thumb_adducted:
+                thumb_opp = j_max
+            else:
+                thumb_opp = j_min
+            joint_angles_t["thumb"] = np.hstack((joint_angles_t["thumb"], [thumb_opp]))
+        all_joint_angles.append(joint_angles_t)
+        all_ee_poses.append(ee_pose)
+    stop_time = time.time()
+    duration = stop_time - start_time
+    time_per_frame = duration / dataset.n_steps
+    frequency = dataset.n_steps / duration
+    print(f"Embodiment mapping done after {duration:.2f} s, "
+          f"{time_per_frame:.4f} s per frame, {frequency:.1f} Hz")
 
-raw_data = []
-for t in range(dataset.n_steps):
-    joint_angles = []
+    pose_columns = ["base_x", "base_y", "base_z", "base_qw", "base_qx", "base_qy", "base_qz"]
+    column_names = []
     for finger in use_fingers:
-        joint_angles += all_joint_angles[t][finger].tolist()
-    pose = pt.pq_from_transform(all_ee_poses[t])
-    raw_data.append(np.hstack((joint_angles, pose)))
+        column_names += hand_config["joint_names"][finger]
+        if args.hand == "mia" and finger == "thumb":
+            column_names.append("j_thumb_opp")
+    column_names += pose_columns
 
-df = pd.DataFrame(raw_data, columns=column_names)
-df.to_csv(args.output)
+    raw_data = []
+    for t in range(dataset.n_steps):
+        joint_angles = []
+        for finger in use_fingers:
+            joint_angles += all_joint_angles[t][finger].tolist()
+        pose = pt.pq_from_transform(all_ee_poses[t])
+        raw_data.append(np.hstack((joint_angles, pose)))
+
+    df = pd.DataFrame(raw_data, columns=column_names)
+    df.to_csv(args.output)
+    print(f"Saved demonstration to '{args.output}'")
+
+
+if __name__ == "__main__":
+    main()
