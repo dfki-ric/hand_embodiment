c1ff2073bffdcfaea9d554ca7e4e927b7c85826d
Alexander Fabisch <afabisch@googlemail.com>
2021-07-15 12:42:51 +0200

    Fix range of index finger

--- hand_embodiment/target_configurations.py
+++ hand_embodiment/target_configurations.py
@@ -10,6 +10,12 @@ from pkg_resources import resource_filename
 
 def kinematic_model_hook_mia(kin):
     """Extends kinematic model to include links for embodiment mapping."""
+    # adjust index finger limit
+    joint_info = kin.tm._joints["j_index_fle"]
+    joint_info = (joint_info[0], joint_info[1], joint_info[2], joint_info[3],
+                  (0.0, joint_info[4][1]), joint_info[5])
+    kin.tm._joints["j_index_fle"] = joint_info
+
     kin.tm.add_transform(
         "thumb_tip", "thumb_fle",
         np.array([
