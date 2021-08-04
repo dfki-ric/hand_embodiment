5f63e41836f0b50e0e2461789cc0ac9cff55a6cc
Alexander Fabisch <afabisch@googlemail.com>
2021-08-04 12:50:56 +0200

    Add function to import exported dataset

---
+++ examples/vis_dataset.py
@@ -0,0 +1,66 @@
+"""
+Example call:
+
+python examples/vis_dataset.py mia ../april_prototype_rl/data/dataset_31/dataset_31_segment_0.csv
+"""
+import argparse
+import time
+import numpy as np
+from pytransform3d import visualizer as pv
+from hand_embodiment.target_dataset import RoboticHandDataset
+from hand_embodiment.target_configurations import TARGET_CONFIG
+from hand_embodiment.embodiment import load_kinematic_model
+
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "hand", type=str,
+        help="Name of the hand. Possible options: mia, shadow_hand")
+    parser.add_argument(
+        "dataset", type=str, help="Dataset that should be used.")
+    parser.add_argument(
+        "--delay", type=float, default=0,
+        help="Delay in seconds before starting the animation")
+    return parser.parse_args()
+
+
+def main():
+    args = parse_args()
+
+    hand_config = TARGET_CONFIG[args.hand]
+    dataset = RoboticHandDataset.import_from_file(args.dataset, hand_config)
+
+    tm = load_kinematic_model(hand_config).tm
+    tm.add_transform("world", hand_config["base_frame"], np.eye(4))
+
+    fig = pv.figure()
+    fig.plot_transform(np.eye(4), s=0.1)
+    graph = pv.Graph(
+        tm, "world", show_frames=True,
+        whitelist=[hand_config["base_frame"]], show_connections=False,
+        show_visuals=True, show_collision_objects=False, show_name=False,
+        s=0.02)
+    graph.add_artist(fig)
+    fig.view_init(azim=45)
+    fig.animate(
+        animation_callback, dataset.n_steps, loop=True,
+        fargs=(dataset, hand_config, tm, graph, args.delay))
+    fig.show()
+
+
+def animation_callback(step, dataset, hand_config, tm, graph, delay):
+    if step == 1:
+        time.sleep(delay)
+
+    tm.add_transform(hand_config["base_frame"], "world", dataset.get_ee_pose(step))
+    finger_joint_angles = dataset.get_finger_joint_angles(step)
+    for finger in hand_config["joint_names"].keys():
+        for i, joint in enumerate(hand_config["joint_names"][finger]):
+            tm.set_joint(joint, finger_joint_angles[finger][i])
+    graph.set_data()
+    return graph
+
+
+if __name__ == "__main__":
+    main()
--- hand_embodiment/target_dataset.py
+++ hand_embodiment/target_dataset.py
@@ -6,6 +6,9 @@ import pandas as pd
 from pytransform3d import transformations as pt
 
 
+POSE_COLUMNS = ["base_x", "base_y", "base_z", "base_qw", "base_qx", "base_qy", "base_qz"]
+
+
 class RoboticHandDataset:
     """Dataset that contains a trajectory of a robotic hand."""
     def __init__(self, finger_names):
@@ -25,13 +28,12 @@ class RoboticHandDataset:
         self.additional_finger_joint_angles[joint_name] = angle
 
     def export(self, filename, hand_config):
-        pose_columns = ["base_x", "base_y", "base_z", "base_qw", "base_qx", "base_qy", "base_qz"]
         column_names = []
         for finger in self.finger_names:
             column_names += hand_config["joint_names"][finger]
         additional_joints = list(sorted(self.additional_finger_joint_angles.keys()))
         column_names += additional_joints
-        column_names += pose_columns
+        column_names += POSE_COLUMNS
 
         raw_data = []
         for t in range(self.n_samples):
@@ -45,6 +47,42 @@ class RoboticHandDataset:
 
         df = pd.DataFrame(raw_data, columns=column_names)
         df.to_csv(filename)
+
+    @staticmethod
+    def import_from_file(filename, hand_config):
+        df = pd.read_csv(filename)
+        ee_poses = [pt.transform_from_pq(df.iloc[t][POSE_COLUMNS])
+                    for t in range(len(df))]
+
+        df.drop(columns=POSE_COLUMNS, inplace=True)
+        df = df[df.columns[1:]]  # drop index
+
+        finger_to_joints = hand_config["joint_names"]
+        finger_names = finger_to_joints.keys()
+
+        finger_joint_angles = [dict() for _ in range(len(df))]
+        for t in range(len(df)):
+            row = df.iloc[t]
+            for finger in finger_names:
+                finger_joint_angles[t][finger] = []
+                for joint in finger_to_joints[finger]:
+                    finger_joint_angles[t][finger].append(row[joint])
+
+        result = RoboticHandDataset(finger_names)
+        result.ee_poses = ee_poses
+        result.finger_joint_angles = finger_joint_angles
+        result.n_samples = len(df)
+        return result
+
+    @property
+    def n_steps(self):  # Compatibility to MotionCaptureDatasetBase
+        return self.n_samples
+
+    def get_ee_pose(self, t):
+        return self.ee_poses[t]
+
+    def get_finger_joint_angles(self, t):
+        return self.finger_joint_angles[t]
 
 
 def convert_mocap_to_robot(dataset, pipeline, ee2origin=None, verbose=0):
