5150940c27f757733817a91cc0ea7ef431773b80
Alexander Fabisch <afabisch@googlemail.com>
2021-11-08 10:46:35 +0100

    Display two electronic components

--- hand_embodiment/vis_utils.py
+++ hand_embodiment/vis_utils.py
@@ -174,35 +174,70 @@ def pillow_pose(pillow_left, pillow_right, pillow_top):
     return pose
 
 
-class ElectronicTarget(pv.Artist):
+class Electronic(pv.Artist):
     """Representation of electronic target component."""
-    def __init__(self, target_top=np.zeros(3), target_bottom=np.array([1, 0, 0])):
-        filename = resource_filename(
+    def __init__(
+            self, target_top=np.zeros(3), target_bottom=np.array([1, 0, 0]),
+            object_left=np.zeros(3), object_right=np.array([0, 1, 0]),
+            object_top=np.array([1, 0, 0])):
+        target_filename = resource_filename(
             "hand_embodiment", "model/objects/electronic_target.stl")
-        self.mesh = o3d.io.read_triangle_mesh(filename)
-        self.mesh.paint_uniform_color(np.array([0.05, 0.05, 0.25]))
-        self.mesh.compute_triangle_normals()
+        self.target_mesh = o3d.io.read_triangle_mesh(target_filename)
+        self.target_mesh.paint_uniform_color(np.array([0.05, 0.05, 0.25]))
+        self.target_mesh.compute_triangle_normals()
+
         self.target_top = np.zeros(3)
         self.target_bottom = np.array([1, 0, 0])
-        self.electronic_target2markers = pt.transform_from(
+
+        self.electronic_target2target_markers = pt.transform_from(
             R=pr.active_matrix_from_extrinsic_roll_pitch_yaw(np.deg2rad([0, 0, 0])),
             p=-np.array([0.0625 + 0.014, -0.057 + 0.006, 0.0]) / 2.0)
-        self.markers2origin = np.copy(self.electronic_target2markers)
-        self.set_data(target_top, target_bottom)
+        self.target_markers2origin = np.copy(self.electronic_target2target_markers)
 
-    def set_data(self, target_top, target_bottom):
+        target_filename = resource_filename(
+            "hand_embodiment", "model/objects/electronic_object.stl")
+        self.object_mesh = o3d.io.read_triangle_mesh(target_filename)
+        self.object_mesh.paint_uniform_color(np.array([1, 0, 0]))
+        self.object_mesh.compute_triangle_normals()
+
+        self.object_left = np.zeros(3)
+        self.object_right = np.array([0, 1, 0])
+        self.object_top = np.array([1, 0, 0])
+
+        self.electronic_object2object_markers = pt.transform_from(
+            R=pr.active_matrix_from_extrinsic_roll_pitch_yaw(np.deg2rad([0, 0, 0])),
+            p=np.array([0.0, 0.0, 0.012]))
+        self.object_markers2origin = np.copy(self.electronic_object2object_markers)
+
+        self.set_data(
+            target_top, target_bottom, object_left, object_right, object_top)
+
+    def set_data(self, target_top, target_bottom, object_left, object_right,
+                 object_top):
         if not any(np.isnan(target_top)):
             self.target_top = target_top
         if not any(np.isnan(target_bottom)):
             self.target_bottom = target_bottom
+        if not any(np.isnan(object_left)):
+            self.object_left = object_left
+        if not any(np.isnan(object_right)):
+            self.object_right = object_right
+        if not any(np.isnan(object_top)):
+            self.object_top = object_top
 
-        self.mesh.transform(pt.invert_transform(pt.concat(
-            self.electronic_target2markers, self.markers2origin)))
-
-        self.markers2origin = electronic_target_pose(
+        self.target_mesh.transform(pt.invert_transform(pt.concat(
+            self.electronic_target2target_markers, self.target_markers2origin)))
+        self.target_markers2origin = electronic_target_pose(
             self.target_top, self.target_bottom)
+        self.target_mesh.transform(pt.concat(
+            self.electronic_target2target_markers, self.target_markers2origin))
 
-        self.mesh.transform(pt.concat(self.electronic_target2markers, self.markers2origin))
+        self.object_mesh.transform(pt.invert_transform(pt.concat(
+            self.electronic_object2object_markers, self.object_markers2origin)))
+        self.object_markers2origin = electronic_object_pose(
+            self.object_left, self.object_right, self.object_top)
+        self.object_mesh.transform(pt.concat(
+            self.electronic_object2object_markers, self.object_markers2origin))
 
     @property
     def geometries(self):
@@ -213,17 +248,28 @@ class ElectronicTarget(pv.Artist):
         geometries : list
             List of geometries that can be added to the visualizer.
         """
-        return [self.mesh]
+        return [self.target_mesh, self.object_mesh]
 
 
 def electronic_target_pose(target_top, target_bottom):
-    """Compute pose of insole."""
+    """Compute pose of electronic target."""
     x_axis = pr.norm_vector(target_top - target_bottom)
     z_axis = np.copy(pr.unitz)
     y_axis = pr.norm_vector(pr.perpendicular_to_vectors(z_axis, x_axis))
     z_axis = pr.norm_vector(pr.perpendicular_to_vectors(x_axis, y_axis))
     R = np.column_stack((x_axis, y_axis, z_axis))
     return pt.transform_from(R=R, p=target_bottom)
+
+
+def electronic_object_pose(object_left, object_right, object_top):
+    """Compute pose of electronic object."""
+    right2top = object_top - object_right
+    right2left = object_left - object_right
+    pose = np.eye(4)
+    pose[:3, :3] = pr.matrix_from_two_vectors(right2top, right2left)
+    object_middle = 0.5 * (object_left + object_right) + 0.5 * right2top
+    pose[:3, 3] = object_middle
+    return pose
 
 
 class AnimationCallback:
@@ -251,7 +297,7 @@ class AnimationCallback:
             self.object_frame.add_artist(self.fig)
 
         if self.args.electronic:
-            self.object_mesh = ElectronicTarget()
+            self.object_mesh = Electronic()
             self.object_mesh.add_artist(self.fig)
             self.object_frame = pv.Frame(np.eye(4), s=0.1)
             self.object_frame.add_artist(self.fig)
@@ -296,9 +342,14 @@ class AnimationCallback:
             additional_markers = dataset.get_additional_markers(t)
             target_top = additional_markers[marker_names.index("target_top")]
             target_bottom = additional_markers[marker_names.index("target_bottom")]
-            if not any(np.isnan(target_bottom)) and not any(np.isnan(target_top)):
-                self.object_mesh.set_data(target_top, target_bottom)
-                artists.append(self.object_mesh)
+            object_left = additional_markers[marker_names.index("object_left")]
+            object_right = additional_markers[marker_names.index("object_right")]
+            object_top = additional_markers[marker_names.index("object_top")]
+            self.object_mesh.set_data(
+                target_top, target_bottom, object_left, object_right,
+                object_top)
+            artists.append(self.object_mesh)
+            if not any(np.isnan(np.hstack((target_bottom, target_top)))):
                 self.object_frame.set_data(
                     electronic_target_pose(target_top, target_bottom))
                 artists.append(self.object_frame)
