32b27af54871b6fd726658c311952cbf50f6bef4
Alexander Fabisch <alexander.fabisch@dfki.de>
2022-03-14 11:12:02 +0100

    Refactor and extend integration test

--- test/test_markers_to_robot.py
+++ test/test_markers_to_robot.py
@@ -3,7 +3,7 @@ from hand_embodiment.mocap_dataset import HandMotionCaptureDataset
 from hand_embodiment.pipelines import MoCapToRobot
 
 
-def test_markers_to_robot():
+def test_markers_to_robot_mia():
     hand = "mia"
     mia_thumb_adducted = True
     interpolate_missing_markers = True
@@ -12,6 +12,28 @@ def test_markers_to_robot():
     mano_config = "examples/config/mano/20210616_april.yaml"
     record_mapping_config = \
         "examples/config/record_mapping/20211105_april.yaml"
+
+    _test_markers_to_robot(
+        hand, demo_file, mocap_config, record_mapping_config, mano_config,
+        interpolate_missing_markers, mia_thumb_adducted)
+
+
+def test_markers_to_robot_shadow():
+    hand = "shadow"
+    interpolate_missing_markers = True
+    demo_file = "test/data/recording.tsv"
+    mocap_config = "examples/config/markers/20210826_april.yaml"
+    mano_config = "examples/config/mano/20210616_april.yaml"
+    record_mapping_config = \
+        "examples/config/record_mapping/20211105_april.yaml"
+
+    _test_markers_to_robot(
+        hand, demo_file, mocap_config, record_mapping_config, mano_config,
+        interpolate_missing_markers)
+
+
+def _test_markers_to_robot(hand, demo_file, mocap_config, record_mapping_config, mano_config,
+                           interpolate_missing_markers, mia_thumb_adducted=None):
     dataset = HandMotionCaptureDataset(
         demo_file, mocap_config=mocap_config,
         skip_frames=100, start_idx=100, end_idx=-1,
@@ -21,8 +43,9 @@ def test_markers_to_robot():
         hand, mano_config, dataset.finger_names,
         record_mapping_config=record_mapping_config, verbose=0)
 
-    angle = 1.0 if mia_thumb_adducted else -1.0
-    pipeline.set_constant_joint("j_thumb_opp_binary", angle)
+    if mia_thumb_adducted is not None:
+        angle = 1.0 if mia_thumb_adducted else -1.0
+        pipeline.set_constant_joint("j_thumb_opp_binary", angle)
 
     for t in range(10):
         hand_markers = dataset.get_hand_markers(t)
