c3cc67089fe289b672383e6a053eafd3cce1f178
Alexander Fabisch <afabisch@googlemail.com>
2022-09-14 00:17:24 +0200

    Inline stuff

--- bin/convert_segments_relative_marker_positions.py
+++ bin/convert_segments_relative_marker_positions.py
@@ -2,15 +2,17 @@
 
 Example call:
 
-    python bin/convert_segments_relative_marker_positions.py close --demo-files raw_data/insole/*.json --mocap-config examples/config/markers/20220328_april.yaml
+    python bin/convert_segments_relative_marker_positions.py close --demo-files examples/eigengrasps/*.json --mocap-config examples/config/markers/20220328_april.yaml --mano-config examples/config/mano/20211105_april.yaml
 """
 import argparse
+import warnings
 
 import numpy as np
+import pytransform3d.transformations as pt
 import tqdm
 
 from hand_embodiment.mocap_dataset import SegmentedHandMotionCaptureDataset
-from hand_embodiment.record_markers import MarkerBasedRecordMapping
+from hand_embodiment.record_markers import MarkerBasedRecordMapping, estimate_hand_pose
 from hand_embodiment.config import load_mano_config, load_record_mapping_config
 from hand_embodiment.command_line import add_configuration_arguments
 
@@ -46,6 +48,8 @@ def main():
 
     finger_names = ["thumb", "index", "middle", "ring", "little"]
 
+    warnings.simplefilter("ignore")
+
     mano2hand_markers, betas = load_mano_config(args.mano_config)
     record_mapping_config = args.record_mapping_config
     if record_mapping_config is not None:
@@ -65,15 +69,40 @@ def main():
         if dataset.n_segments == 0:
             continue
 
-        for i in range(dataset.n_segments):
+        for i in tqdm.tqdm(range(dataset.n_segments), leave=False):
             dataset.select_segment(i)
 
             for t in range(dataset.n_steps):
-                mbrm.make_finger_markers_relative(
-                    dataset.get_hand_markers(t), dataset.get_finger_markers(t))
+                hand_markers = dataset.get_hand_markers(t)
+                finger_markers = dataset.get_finger_markers(t)
+
+                current_hand_markers2world = estimate_hand_pose(*hand_markers)
+                if np.any(np.isnan(current_hand_markers2world)):
+                    continue
+
+                mano2world_ = pt.concat(
+                    mbrm.mano2hand_markers_, current_hand_markers2world)
+                world2mano = pt.invert_transform(mano2world_, check=False)
+
                 sample = []
+                markers_in_mano = {}
                 for finger_name in finger_names:
-                    sample.extend(mbrm.markers_in_mano_[finger_name].ravel().tolist())
+                    markers_in_world = np.atleast_2d(finger_markers[finger_name])
+                    markers_in_mano[finger_name] = np.dot(
+                        pt.vectors_to_points(markers_in_world), world2mano.T)[:, :3]
+                    sample.extend(markers_in_mano[finger_name].ravel())
+
+                """
+                import pytransform3d.visualizer as pv
+                fig = pv.figure()
+                mbrm.estimate(hand_markers, finger_markers)
+                fig.add_geometry(mbrm.hand_state_.hand_mesh)
+                for finger_name in finger_markers:
+                    fig.scatter(finger_markers[finger_name], s=0.006)
+                fig.scatter(hand_markers, s=0.006)
+                fig.show()
+                """
+
                 output_dataset.append(sample)
 
     output_dataset = np.array(output_dataset)
